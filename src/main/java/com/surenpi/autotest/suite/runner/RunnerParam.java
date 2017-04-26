4369088c6e510f6f04ce07137ee61eef750f6a9c
suren <suren@0b0a54f1-932d-4d7b-9bbf-d20aa1ac5a98>
2017-04-26 13:33:46 +0000

    project import

---
+++ src/main/java/com/surenpi/autotest/suite/runner/RunnerParam.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.surenpi.autotest.suite.runner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.beust.jcommander.Parameter;
+
+/**
+ * 测试套件运行参数
+ * @author suren
+ * @date 2017年4月26日 下午5:01:34
+ */
+public class RunnerParam
+{
+	@Parameter(names = "-list", description = "列出所有的测试套件")
+	private boolean listRunners = false;
+	
+	@Parameter(names = "-runners", description = "指定运行的测试套件列表")
+	private List<String> runners = new ArrayList<String>();
+	
+	@Parameter(names = "-download-driver", description = "指定要下载的浏览器驱动以及版本号，例如：chrome.57")
+	private String downloadDriver;
+	
+	@Parameter(names = "-download-dir", description = "指定下载后的目录")
+	private String downloadDir;
+}
