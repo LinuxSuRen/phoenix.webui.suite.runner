df3a1a144cf559070291327d54b22a6073d0ee1a
surenpi <zxjlwt@126.com>
2017-12-02 21:36:52 +0800

    增加设置解析类的参数

---
+++ src/test/java/autotest/suite/runner/SuiteRunnerLauncherTest.java
@@ -0,0 +1,15 @@
+package autotest.suite.runner;
+
+import com.surenpi.autotest.suite.SuiteRunnerLauncher;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class SuiteRunnerLauncherTest
+{
+    @Test
+    public void runners() throws IOException
+    {
+        SuiteRunnerLauncher.main(new String[]{"-runners", "target/test-classes/test.xml"});
+    }
+}
